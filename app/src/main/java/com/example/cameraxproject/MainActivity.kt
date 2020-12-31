91e7adc4b4cea4e539f083f393e35271dafc3ccf
takieddine12 <algeriahero22@gmail.com>
2020-12-31 05:14:08 +0100

    12/31/2020 5:14 AM

--- app/src/main/java/com/example/cameraxproject/MainActivity.kt
+++ app/src/main/java/com/example/cameraxproject/MainActivity.kt
@@ -1,12 +1,72 @@
 package com.example.cameraxproject
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private val imageCapture : ImageCapture? = null
+    private lateinit var fileDirectory : File
+    private lateinit var cameraExcecutor : ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        if(allPermissionsGranted()){
+            startCamera()
+        } else {
+            ActivityCompat
+                    .requestPermissions(
+                            this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+        camera_capture_button.setOnClickListener {
+            takePhoto()
+        }
+
+        fileDirectory = getOutputDirectory()
+        cameraExcecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto() {
+
+    }
+
+    private fun startCamera() {
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExcecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
